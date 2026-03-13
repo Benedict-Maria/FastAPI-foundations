c49c3100079d74d611544ec256bf9398b495704c
Benedict Maria L <162155333+Benedict-Maria@users.noreply.github.com>
2026-03-13 14:16:12 +0530

    Updated database.py

--- student_sqlalchemy_project/database.py
+++ student_sqlalchemy_project/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:%28ct3_1_7ia%29%2A@localhost:3306/studentdb"
+DATABASE_URL = "mysql+pymysql://root:mypassword@localhost:3306/studentdb"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
